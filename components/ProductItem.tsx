15c9e34b10d5de1b1a254a1fc9d7e260be89e32a
Brenno Rodrigues de Assis <brennorassis@gmail.com>
2021-12-14 18:58:26 -0300

    Criando projeto

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,3 +1,5 @@
+import { memo } from 'react'
+
 interface ProductItemProps { 
     product: {
         id:number;
@@ -7,7 +9,7 @@ interface ProductItemProps {
 }
 
 
-export const ProductItem = ({ product }: ProductItemProps) => {
+ const ProductItemComponent = ({ product }: ProductItemProps) => {
     return (
         <div>
             {product.title} - <strong> {product.price} </strong>
@@ -15,3 +17,13 @@ export const ProductItem = ({ product }: ProductItemProps) => {
     )
     
 }
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+    return Object.is(prevProps.product,nextProps.product)
+});
+/**
+ * Quando usar o memo
+ * 1.Quando o componente é um pure functional component, ou seja, se o componente receber sempre a mesma prop, ele vai devolver sempre o mesmo html
+ * 2.Quando a prop é sempre a mesma, quando não tem uma api que muda a prop de tempos em tempos ou algo que dependa do horário
+ * 3.Quando o componente é re-renderizado muitas e muitas vezes
+ * 4.Quando o componente tem de médio a grande sizing
+ */
