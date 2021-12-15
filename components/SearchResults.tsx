a468816eb2bbdb69661dc8919e0cb4d3dc66c9aa
Brenno Rodrigues de Assis <brennorassis@gmail.com>
2021-12-15 08:46:19 -0300

    Explicações sobre novos hooks

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,3 +1,5 @@
+
+import { useMemo } from 'react';
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
@@ -8,13 +10,27 @@ interface SearchResultsProps {
     }>
 }
 export const SearchResults = ({ results }: SearchResultsProps) => {
+    const totalPrice = useMemo(() => {
+       return results.reduce((total, product) => {
+            return total +  product.price
+        }, 0) 
+    },[results])
+
     return  (
         <div>
+            <h2>{totalPrice}</h2>
             {results.map(product => {
                 return (
-                    <ProductItem product={product} />
+                    <ProductItem key={product.id} product={product} />
                 );
             })}
         </div>
     )
 }
+/**
+ * useMemo / useCallback
+ * 1. Usar em casos em que há Calculos pesados.
+ * 2. Caso a funcionalidade/variável que o utiliza o useMemo seja passada para o componente filho, o useMemo também evita que essa variável ocupe um novo espaço na memória.
+ * 3. No caso do useCallback a este hook apenas memoriza uma função. O hook é utilizado em situações onde a função será repassada como prop para um componente filho
+ * A diferença entre ambos os hooks está no fato de "useMemo" memorizar um resultado e useCallback uma function.
+ */
